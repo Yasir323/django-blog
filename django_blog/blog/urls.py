bc0cb5e6801379b64e8a412d0f0026362b2609c3
Yasir323 <63918234+Yasir323@users.noreply.github.com>
2021-03-05 11:50:07 +0530

    Pagination added

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+urlpatterns = [
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # We are using dynamic routing where pk is the primary key (ID) of the post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    # This time name the file post_form.html, this will be shared by create and update views.
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+]
---
+++ django_blog/blog/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+# from django.http import HttpResponse
+
+# posts = [
+#     {
+#         'author': 'Luffy',
+#         'title': 'Blog Post 1',
+#         'content': 'I am gonna be the King of the Pirates!',
+#         'date_posted': 'August 20, 2017'
+#     },
+#     {
+#         'author': 'Zoro',
+#         'title': 'Blog Post 2',
+#         'content': 'I will become the world\'s strongest swordsman!',
+#         'date_posted': 'July 20, 2021'
+#     }
+# ]
+
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    # Class based views look for templates like these: <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # Sorting blogs by latest to oldest
+    paginate_by = 5
+
+
+
+class PostDetailView(DetailView):
+    model = Post
+    # So let's create a template with the naming conventions
+    # It will be blog/post_detail.html
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class UserPostListView(ListView):
+    model = Post
+    # Class based views look for templates like these: <app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    # ordering = ['-date_posted']  # Sorting blogs by latest to oldest
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
